5f84f547237f3ea34cf758c3bda1970d7f0749fd
PULKIT SHARMA <spulkit489@gmail.com>
2025-05-26 12:10:34 +0530

    changed implementation with locks and done channel

--- controller.go
+++ controller.go
@@ -2,9 +2,7 @@ package broadcastchannels
 
 import (
 	"broadcast-channels/constants"
-	identitylocks "broadcast-channels/identityLocks"
 	"errors"
-	"fmt"
 	"sync"
 
 	"github.com/google/uuid"
@@ -16,7 +14,7 @@ More functions for support in future :
 */
 type BroadcastInterface[T any] interface {
 	/*Id is returned for getting listeners, */
-	Subscribe() (string, error)
+	Subscribe() (string, *Listener[T], error)
 	/*
 		This closes broadcast channels, effectively closing all listeners as well.
 		Make sure you are consuming listeners after checking their aliveness.
@@ -34,43 +32,59 @@ type BroadcastInterface[T any] interface {
 
 	*/
 	Broadcast(T) error
-	/*
-		Get Listener by id. id is generated at the time of subscription.
-		in case id does not exist, or Close has been called on broadcast channel, this will return a nil channel.
-		Note : checking for nil-ness is important, as it will block forever if read from, unchecked.
-	*/
-	Listener(id string) (<-chan T, error)
+}
+
+type Listener[T any] struct {
+	DoneChan <-chan struct{}
+	DataChan <-chan T
+}
+
+type internalListener[T any] struct {
+	doneChan chan struct{}
+	dataChan chan T
 }
 
 type broadcastChannel[T any] struct {
-	chans     sync.Map                    // map that will contain all the channels who have subscribed to this channel
-	buffer    int                         // buffer for channels
-	isClosed  bool                        // tracks the request of closing of broadcast channel
-	closeLock *identitylocks.IdentityLock // lock for operations
-	singleOp  sync.Once                   // for closing broadcast channel only once even if multiple process call it
+	chans     sync.Map   // map that will contain all the channels who have subscribed to this channel
+	buffer    int        // buffer for channels
+	isClosed  bool       // tracks the request of closing of broadcast channel
+	closeLock sync.Mutex // lock for operations
+	singleOp  *sync.Once // for closing broadcast channel only once even if multiple process call it
 }
 
-// Use Id returned by this function to get particular listener
-func (b *broadcastChannel[T]) Subscribe() (string, error) {
+/*
+Returns Id, listener and error if any.
+Id : A unique identifier for this particular subscription.
+Listner which
+*/
+func (b *broadcastChannel[T]) Subscribe() (string, *Listener[T], error) {
 
 	if b.isClosed {
-		return "", errors.New(constants.MessageClosedChannel)
+		return "", nil, errors.New(constants.MessageClosedChannel)
 	}
 
-	if locked, id := b.closeLock.IsLocked(); locked && id == constants.CloserId {
-		// close lock is held. that means, channel is being closed. We cannot subscribe to a closing channel
-		// lock should be held by closer
-		return "", errors.New(constants.MessageClosedChannel)
-	}
+	b.closeLock.Lock()
+	defer b.closeLock.Unlock()
 
 	id := uuid.New().String()
-	b.chans.Store(id, make(chan T, b.buffer))
-	return id, nil
+
+	dataChan := make(chan T, b.buffer)
+	doneChan := make(chan struct{})
+	b.chans.Store(id, &internalListener[T]{
+		doneChan: doneChan,
+		dataChan: dataChan,
+	})
+	return id, &Listener[T]{
+		DoneChan: doneChan,
+		DataChan: dataChan,
+	}, nil
 }
 
 // Closes the channel
 func (b *broadcastChannel[T]) Close() {
 
+	b.closeLock.Lock()
+	defer b.closeLock.Unlock()
 	if b.isClosed {
 		return
 	}
@@ -78,12 +92,13 @@ func (b *broadcastChannel[T]) Close() {
 
 	// should be run only once even if race occurs to close the channel
 	b.singleOp.Do(func() {
-		b.closeLock.Lock(constants.CloserId)
-		defer b.closeLock.Unlock()
 		b.chans.Range(func(key, value any) bool {
-			c := value.(chan T)
+			c := value.(*internalListener[T])
 			if c != nil {
-				close(c)
+				// inform for graceful shutdown
+				go func() { c.doneChan <- struct{}{}; close(c.doneChan) }()
+				// close the data channel
+				close(c.dataChan)
 			}
 			return true
 		})
@@ -92,49 +107,48 @@ func (b *broadcastChannel[T]) Close() {
 }
 
 func (b *broadcastChannel[T]) Unsubscribe(id string) {
+	b.closeLock.Lock()
+	defer b.closeLock.Unlock()
+
+	if b.isClosed {
+		return
+	}
+
 	v, ok := b.chans.LoadAndDelete(id)
 	if !ok {
 		return
 	}
 
-	close(v.(chan T))
+	l := v.(*internalListener[T])
+	if l == nil {
+		return
+	}
+
+	// for graceful shutdown of listner
+	l.doneChan <- struct{}{}
+
+	close(l.doneChan)
+	close(l.dataChan)
 }
 
 func (b *broadcastChannel[T]) Broadcast(data T) error {
+
+	b.closeLock.Lock()
+	defer b.closeLock.Unlock()
 
 	if b.isClosed {
 		return errors.New(constants.MessageClosedChannel)
 	}
 
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(r)
-		}
-	}()
-
 	b.chans.Range(func(key, value any) bool {
-		c, ok := value.(chan T)
-		if ok {
-			c <- data
+		l, ok := value.(*internalListener[T])
+		if ok && l != nil {
+			l.dataChan <- data
 		}
 		return true
 	})
 
 	return nil
-}
-
-// If there is no subscription with provided id, then it will return nil, hence nil check is necessary
-func (b *broadcastChannel[T]) Listener(id string) (<-chan T, error) {
-
-	if b.isClosed {
-		return nil, errors.New("closed channel")
-	}
-
-	v, ok := b.chans.Load(id)
-	if !ok {
-		return nil, errors.New("channel not found")
-	}
-	return v.(chan T), nil
 }
 
 // Accepts optional buffer size for channel, if multiple buffers are provided
@@ -149,7 +163,7 @@ func NewBroadcastChannel[T any](buffer ...int) BroadcastInterface[T] {
 		buffer:    b,
 		chans:     sync.Map{},
 		isClosed:  false,
-		closeLock: identitylocks.NewIdentityLock(),
-		singleOp:  sync.Once{},
+		closeLock: sync.Mutex{},
+		singleOp:  &sync.Once{},
 	}
 }
